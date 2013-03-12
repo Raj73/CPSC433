e31144023f15a6786e3cb5e49cd2ed5d874e9c65
Raj73 <rajansaini73@hotmail.com>
2013-03-12 02:02:08 -0600

    Update Room.java

--- Room.java
+++ Room.java
@@ -8,7 +8,7 @@ public class Room extends Entity
 	boolean medium;
 	boolean large;
 
-	LinkedList<String> close;
+	LinkedList<String> closeWith;
 
 	// Room qualities
 
@@ -42,8 +42,8 @@ public class Room extends Entity
 		return false;
 	}
 
-  // Medium Room asserts and qeuries
-  void assertMedium(String Rname)
+  	// Medium Room asserts and qeuries
+  	void assertMedium(String Rname)
 	{
 		if (name.compareTo(Rname) == 0)
 			medium = true;
@@ -57,8 +57,8 @@ public class Room extends Entity
 		return false;
 	}
 
-  // Large Room asserts and qeuries
-  void assertLarge(String Rname)
+  	// Large Room asserts and qeuries
+ 	void assertLarge(String Rname)
 	{
 		if (name.compareTo(Rname) == 0)
 			large = true;
@@ -72,4 +72,48 @@ public class Room extends Entity
 		return false;
 	}
 
-  
+  	// Works with asserts and qeuries (sets)
+	void assertCloseWith(String Rname, TreeSet<Pair<ParamType,Object>> r2s)
+	{
+		if (name.compareTo(Rname) == 0)
+		{
+			int size = r2s.size();
+			Pair input = r2s.pollfirst();
+
+			for (i = 0; i < size; i++)
+			{
+				closeWith.add(input.getValue());
+				Pair input = r2s.pollfirst();
+			}
+		}
+	}
+
+	boolean evaluateWorksWith(String Rname, TreeSet<Pair<ParamType,Object>> r2s)
+	{
+		if (name.compareTo(Rname) == 0)
+		{
+			int inputSize = r2s.size();
+			int currentSize = closeWith.size();
+			int found = 0;
+			Pair input = r2s.pollfirst();
+
+			for (i = 0; i < inputSize; i++)
+			{
+				for (j = 0; j < currentSize; j++)
+				{
+					if (closeWith.get(j).compareTo(input.getValue()) == 0)
+					{
+						found++;
+						continue;
+					}
+				}
+				Pair input = r2s.pollfirst();
+			}
+
+			if (found == inputSize)
+				return true;
+		}
+
+		return false;
+	}
+}
